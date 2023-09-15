07b0e03ff7e7ccd7f6dac54c4cb8532075f5fbe7
keenlight96 <nguyenkimlong29196@gmail.com>
2023-09-15 13:57:07 +0700

    add status 6

--- src/main/java/com/repository/IBillRepository.java
+++ src/main/java/com/repository/IBillRepository.java
@@ -22,7 +22,7 @@ public interface IBillRepository extends JpaRepository<Bill, Long> {
 //    @Query(nativeQuery = true, value = "select * from bill  where accountccdv_id= :accountccdv_id  ")
 //    List<Bill> getAllBillByAccountCCDV(@Param("accountccdv_id") long accountccdv_id);
 
-    @Query(nativeQuery = true, value = "select * from bill  where (status_id= 5 or status_id= 4 )and accountCCDV_id=:id and ( date_start   between  :#{#dayNow} and DATE_ADD(:#{#dayNow}, INTERVAL 7 DAY) )")
+    @Query(nativeQuery = true, value = "select * from bill  where (status_id= 5 or status_id= 4 or status_id = 6)and accountCCDV_id=:id and ( date_start   between  :#{#dayNow} and DATE_ADD(:#{#dayNow}, INTERVAL 7 DAY) )")
     Optional<List<Bill>> getAllBill7DayByID(@Param("id") long id,@Param("dayNow")String dayNow);
 
     Optional<List<Bill>> getAllByAccountCCDV_Id(long accountId, Sort sort);
